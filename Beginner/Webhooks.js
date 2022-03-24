522ebfb1e79740575544a98a16472f85bb1a7ef6
thenorthstar <68077608+thenorthstar@users.noreply.github.com>
2022-03-24 22:34:54 +0300

    Create Webhooks.js

---
+++ Beginner/Webhooks.js
@@ -0,0 +1,30 @@
+var room = HBInit({ roomName: "TEST", noPlayer:true, public:true, maxPlayers: 30});
+
+function sendAnnouncementToDiscord(message) {
+
+    var request = new XMLHttpRequest();
+    request.open("POST","https://discord.com/api/webhooks/...");
+
+    request.setRequestHeader('Content-type', 'application/json');
+
+    var params = {
+        avatar_url: '',
+        username: 'Your Discord Webhook',
+        content: message
+    };
+
+    request.send(JSON.stringify(params));
+}
+
+room.onPlayerJoin = function(player){
+    console.log(player.name + " has joined.");
+}
+
+room.onPlayerLeave = function(player){
+    console.log(player.name + " has left.");
+}
+
+room.onPlayerChat = function(player,msg){
+    console.log(player.name + ": " + msg);
+    sendAnnouncementToDiscord(msg);
+};
